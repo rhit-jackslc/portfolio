97179c3b4c36710ff010bb8bde694f85fa2df0d5
rhit-jackslc <jackslc@rose-hulman.edu>
2025-04-19 16:18:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,11 @@ document.getElementById('theme-toggle').addEventListener('click', function() {
   }
 });
 
-fetch('https://api.github.com/users/rhit-jackslc/repos')
-  .then(response => response.json())
-  .then(repos => {
-    const list = document.getElementById('repo-list');
-    repos.forEach(repo => {
-      const li = document.createElement('li');
-      li.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>: ${repo.description || ''}`;
-      list.appendChild(li);
-    });
-  })
-  .catch(err => {
-    document.getElementById('repo-list').innerText = 'Could not load GitHub projects.';
-  });
-
+document.getElementById('theme-toggle').addEventListener('click', function() {
+  try {
+    document.body.classList.toggle('dark-mode');
+  } catch (e) {
+    alert('Sorry, theme toggle failed.');
+    console.error(e);
+  }
+});
