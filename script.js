8be119d4237a655798a9bd16fc603073c0c99fcf
rhit-jackslc <jackslc@rose-hulman.edu>
2025-04-19 16:27:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ document.getElementById('theme-toggle').addEventListener('click', function() {
     console.error(e);
   }
 });
-
-document.getElementById('theme-toggle').addEventListener('click', function() {
-  try {
-    document.body.classList.toggle('dark-mode');
-  } catch (e) {
-    alert('Sorry, theme toggle failed.');
-    console.error(e);
-  }
-});
